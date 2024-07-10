ad4548581baa4b693d9e21634515a7bed109fbdb
Anika Stephen Wilbur <anika.m.stephen@gmail>
2024-07-09 20:59:04 -0700

    Complete part 4 of Using the OpenAI API RT activity

--- app/routes/pet_routes.py
+++ app/routes/pet_routes.py
@@ -21,18 +21,43 @@ def add_pets():
 
         db.session.add(new_pet)
         db.session.commit()
-
+        
         return make_response("New pet created", 201)
     
     except KeyError as e:
         abort(make_response({"message": f"missing required value: {e}"}, 400))
 
-# helper function
+@bp.patch("/<pet_id>")
+def change_pet_name(pet_id):
+
+    pet = Pet.query.get(pet_id)
+    pet.name = generate_new_pet_name(pet)
+    db.session.commit()
+
+    return make_response("Pet name updated", 200)
+    
+
+# helper functions
 def generate_pet_name(new_pet):
     prompt = f"I am creating a library of various pets and I need help naming them! \
         I have one pet that is a {new_pet['coloring']} {new_pet['animal type']} \
-        with a {new_pet['personality']} personalitly. Please generate \
+        with a {new_pet['personality']} personality. Please generate \
         a name that would suit this special animal!"
+
+    completion = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return(completion.choices[0].message.content)
+
+
+def generate_new_pet_name(pet):
+    prompt = f"{pet.name} the {pet.animal_type} isn't happy with its name. \
+        Can you create a new name for {pet.name} based on its {pet.color} color, \
+        {pet.personality} personality, and the type of animal it is?"
 
     completion = client.chat.completions.create(
         model = "gpt-3.5-turbo",
